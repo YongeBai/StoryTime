5aa035bdbc7183a29fe4f0cd5dac7cebd64654fe
Simon Lin <lin.simon2003@gmail.com>
2023-08-12 15:08:56 -0400

    change font

--- sv.py
+++ sv.py
@@ -3,10 +3,10 @@ import customtkinter as ct
 import sv_ttk
 
 from tkinter import ttk
-from customtkinter import CTkFont
 from commands import upload, callback, center
 
 window = tk.Tk()
+style = ttk.Style()
 
 window.eval('tk::PlaceWindow . center')
 window.geometry("640x500")
@@ -15,11 +15,14 @@ window.title("audible")
 
 sv_ttk.set_theme("dark")
 
-label = ttk.Label(window, text="Paste Link to an ePub:", font=CTkFont(slant="roman",size=20), anchor='center')
+label = ttk.Label(window, text="Paste Link to an ePub:", style="label.TLabel", anchor='center')
 link = ttk.Entry(window, width=40)
-submit = ttk.Button(window, text="Submit", command=lambda:callback(link))
-or_txt = ttk.Label(window, text=" or ", anchor='center', font=CTkFont(slant="italic",size=16))
-epub_upload = ttk.Button(window, text='Upload an ePub (.epub)', command=lambda:upload())
+submit = ttk.Button(window, text="Submit", style="Accent.TButton", command=lambda:callback(link))
+or_txt = ttk.Label(window, text="or", anchor='center', style="label.TLabel" )
+epub_upload = ttk.Button(window, text='Upload an ePub (.epub)', style="Accent.TButton", command=lambda:upload())
+
+style.configure('submit.TButton', font=('Consolas', 12))
+style.configure('label.TLabel', font=('Consolas', 16))
 
 label.place(
             relx=.5,
