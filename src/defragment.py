097c6395f946b810afb4fd80f88743b3ac97b4c3
Simon Lin <lin.simon2003@gmail.com>
2023-08-19 18:01:09 -0400

    implement utf-8 encoding & regex defragmenting

--- src/defragment.py
+++ src/defragment.py
@@ -1,1 +1,7 @@
-#TODO: remove image fragments from output files
+import re
+
+with open('output.txt', encoding='utf-8') as f: 
+    fragments = f.read()
+
+with open('output.txt', 'w') as f:
+    f.write(re.sub('[^a-zA-Z0-9\n\.]', ' ', fragments))
